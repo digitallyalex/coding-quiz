e7b16d87bc5b864580f6c43642d2ce8301bf7f30
Alexandra Asanache <ale.asanache@gmail.com>
2023-12-11 14:02:27 +0000

    Updated with final questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,32 +2,63 @@
 
 const questions = [
   {
-    question: "What colour is the sky?",
-    options: ["<yellow>", "<blue>", "<red>", "<green>"],
-    answer: "<blue>",
+    question:
+      "What is the correct way to check if a variable x is of type 'string' in JavaScript?",
+    options: [
+      "if (x === 'string')",
+      "if (typeOf x === 'string'",
+      "if (typeof x === 'string')",
+      "if (x.type === 'string')",
+    ],
+    answer: "if (typeof x === 'string')",
   },
 
   {
-    question: "How many days in a week?",
-    options: ["<three>", "<five>", "<one>", "<seven>"],
-    answer: "<seven>",
+    question: "How do you convert a string '123' to a number in JavaScript?",
+    options: [
+      "parseInt('123')",
+      "convertNumber('123')",
+      "toNumber('123')",
+      "number('123')",
+    ],
+    answer: "parseInt('123')",
   },
 
   {
-    question: "What's the best chocolate?",
-    options: ["<90%>", "<55%>", "<70%>", "<80%>"],
-    answer: "<90%>",
+    question: "Which HTML element is used to put the JavaScript code?",
+    options: ["<javascript>", "<js>", "<scripting>", "<script>"],
+    answer: "<script>",
   },
 
   {
-    question: "What colour is a banana?",
-    options: ["<blue>", "<green>", "<pink>", "<yellow>"],
-    answer: "<yellow>",
+    question: "Which of the following is not a JavaScript Data Types?",
+    options: ["Boolean", "String", "Float", "Number"],
+    answer: "Float",
   },
 
   {
-    question: "Who is the red head in RBD?",
-    options: ["<Maite>", "<Dulce>", "<Anahi>", "<Chris>"],
-    answer: "<Dulce>",
+    question:
+      "Which of the following JavaScript operator is used to assign a value to a variable based on some condition?",
+    options: [
+      "Assignment operator",
+      "Bitwise operator",
+      "Logical operator",
+      "Conditional operator",
+    ],
+    answer: "Conditional operator",
+  },
+
+  {
+    question:
+      "Which of the following function of String object splits a String object into an array of strings by separating the string into substrings?",
+    options: ["slice()", "split()", "replace()", "search()"],
+    answer: "split()",
+  },
+
+  {
+    question:
+      "Which of the following attribute is used to include external js code inside your HTML document?",
+    options: ["src", "external", "script", "link"],
+    answer: "src",
   },
 ];
